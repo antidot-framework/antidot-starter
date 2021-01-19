804230a8b6a5c295ed466f462384cb058e4ac815
peter279k <peter279k@gmail.com>
2021-01-19 19:50:32 +0800

    Improving setup routes comments

--- router/routes.php
+++ router/routes.php
@@ -10,15 +10,11 @@ use Psr\Container\ContainerInterface;
 /**
  * Setup routes with a single request method:
  *
- * $app->get('/', App\Handler\HomePageHandler::class, 'home');
- * $app->post('/album', App\Handler\AlbumCreateHandler::class, 'album.create');
- * $app->put('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.put');
- * $app->patch('/album/:id', App\Handler\AlbumUpdateHandler::class, 'album.patch');
- * $app->delete('/album/:id', App\Handler\AlbumDeleteHandler::class, 'album.delete');
- *
- * Or with multiple request methods:
- *
- * $app->route('/contact', App\Handler\ContactHandler::class, ['GET', 'POST', ...], 'contact');
+ * $app->get('/', [App\Handler\HomePageHandler::class], 'home');
+ * $app->post('/album', [App\Handler\AlbumCreateHandler::class], 'album.create');
+ * $app->put('/album/:id', [App\Handler\AlbumUpdateHandler::class], 'album.put');
+ * $app->patch('/album/:id', [App\Handler\AlbumUpdateHandler::class], 'album.patch');
+ * $app->delete('/album/:id', [App\Handler\AlbumDeleteHandler::class], 'album.delete');
  */
 return static function (Application $app, ContainerInterface $container) : void {
     $app->get('/', [SomeMiddleware::class, HomePage::class], 'home');
