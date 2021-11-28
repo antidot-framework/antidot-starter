d8e83c6293067b104652c5cd4df4c2d74fc9ccc6
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-11-28 11:08:13 +0100

    update to use new antidot framework package

--- src/Application/Http/Handler/HomePage.php
+++ src/Application/Http/Handler/HomePage.php
@@ -6,28 +6,36 @@ namespace App\Application\Http\Handler;
 
 use App\Application\Event\SomeEvent;
 use Psr\EventDispatcher\EventDispatcherInterface;
+use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Laminas\Diactoros\Response\JsonResponse;
 
 class HomePage implements RequestHandlerInterface
 {
     /** @var EventDispatcherInterface */
     private $eventDispatcher;
+    private ResponseFactoryInterface $responseFactory;
 
-    public function __construct(EventDispatcherInterface $eventDispatcher)
-    {
+    public function __construct(
+        EventDispatcherInterface $eventDispatcher,
+        ResponseFactoryInterface $responseFactory
+    ) {
         $this->eventDispatcher = $eventDispatcher;
+        $this->responseFactory = $responseFactory;
     }
 
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
         $this->eventDispatcher->dispatch(SomeEvent::occur());
 
-        return new JsonResponse([
+        $response = $this->responseFactory->createResponse();
+        $response = $response->withAddedHeader('Content-Type', 'application/json');
+        $response->getBody()->write(json_encode([
             'docs' => 'https://antidotfw.io',
             'Message' => 'Welcome to Antidot Framework Starter'
-        ]);
+        ], JSON_THROW_ON_ERROR));
+
+        return $response;
     }
 }
