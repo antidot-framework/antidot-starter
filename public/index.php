06209305e60cde68601943c46a79a29b181e969a
Koldo Picaza <1093654+kpicaza@users.noreply.github.com>
2021-11-28 18:33:06 +0100

    Use new Antidot Runtime component

--- public/index.php
+++ public/index.php
@@ -3,34 +3,22 @@
 declare(strict_types=1);
 
 use Antidot\Framework\Application;
-use Antidot\Framework\Middleware\MiddlewareFactory;
-use Antidot\Framework\Router\RouteFactory;
-use Antidot\Framework\Router\Router;
-use Antidot\Framework\Runtime\AntidotRuntime;
+use Antidot\Runtime\AntidotRuntime;
 
 ini_set('memory_limit', '2048M');
 
 $_SERVER['APP_RUNTIME'] = AntidotRuntime::class;
-$_SERVER['APP_RUNTIME_OPTIONS'] = [
-    'host' => '0.0.0.0',
-    'port' => 3000,
-    'debug' => true,
-];
 $rootDir = dirname(__DIR__);
 
 require_once $rootDir . '/vendor/autoload_runtime.php';
 
 return static function () use ($rootDir) {
     $container = require $rootDir . '/config/container.php';
-    $middlewareFactory = $container->get(MiddlewareFactory::class);
-    $application = new Application(
-        $middlewareFactory,
-        new RouteFactory(),
-        $container->get(Router::class)
-    );
+    $application = $container->get(Application::class);
+
     (require $rootDir . '/router/middleware.php')($application);
     (require $rootDir . '/router/routes.php')($application, $container);
 
-    return $application;
+    return $container;
 };
 
